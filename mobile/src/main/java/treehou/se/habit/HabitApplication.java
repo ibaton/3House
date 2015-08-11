4e6c5c8b20e1e7e3b675c7f870838e1efa1e7ac5
Mattias Markehed <mattias.markehed@gmail.com>
2015-08-11 14:10:59 +0200

    Fabric

--- mobile/src/main/java/treehou/se/habit/HabitApplication.java
+++ mobile/src/main/java/treehou/se/habit/HabitApplication.java
@@ -3,6 +3,8 @@ package treehou.se.habit;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import treehou.se.habit.connector.TrustModifier;
 
 public class HabitApplication extends com.activeandroid.app.Application {
@@ -10,6 +12,7 @@ public class HabitApplication extends com.activeandroid.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         // TODO Remove when support for self signed certificates
         TrustModifier.NukeSSLCerts.nuke();
