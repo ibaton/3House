3ef3a70e85b38e0d54a0cc21f60cb00993a3c3f3
ibaton <mattias.markehed@gmail.com>
2016-08-10 16:05:43 +0200

    Added rollershutter test. Added matcher to check seekbar value.

--- mobile/src/androidTest/java/treehou/se/habit/ViewActions/SliderActions.java
+++ mobile/src/androidTest/java/treehou/se/habit/ViewActions/SliderActions.java
@@ -2,10 +2,12 @@ package treehou.se.habit.ViewActions;
 
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.view.View;
 import android.widget.SeekBar;
 
+import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 
 public class SliderActions {
@@ -26,6 +28,22 @@ public class SliderActions {
             @Override
             public Matcher<View> getConstraints() {
                 return ViewMatchers.isAssignableFrom(SeekBar.class);
+            }
+        };
+    }
+
+    public static Matcher<View> withProgress(final int expectedProgress) {
+
+        return new BoundedMatcher<View, SeekBar>(SeekBar.class) {
+            @Override
+            protected boolean matchesSafely(SeekBar seekBar) {
+                return seekBar.getProgress() == expectedProgress;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("expected: ");
+                description.appendText(""+expectedProgress);
             }
         };
     }
