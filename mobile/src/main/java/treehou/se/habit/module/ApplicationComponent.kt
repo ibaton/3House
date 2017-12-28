5baef3c3b8f4416efaf366f8dee15639e1e89deb
ibaton <mattias.markehed@gmail.com>
2017-12-28 22:09:37 +0100

    Converted application component to kotlin

---
+++ mobile/src/main/java/treehou/se/habit/module/ApplicationComponent.kt
@@ -0,0 +1,98 @@
+package treehou.se.habit.module
+
+import dagger.Component
+import treehou.se.habit.BaseActivity
+import treehou.se.habit.HabitApplication
+import treehou.se.habit.RestartBroadcastReceiver
+import treehou.se.habit.service.wear.VoiceActionService
+import treehou.se.habit.tasker.items.CommandActionFragment
+import treehou.se.habit.tasker.items.IncDecActionFragment
+import treehou.se.habit.tasker.items.SwitchActionFragment
+import treehou.se.habit.tasker.reciever.CommandReciever
+import treehou.se.habit.tasker.reciever.IncDecReciever
+import treehou.se.habit.ui.BaseDialogFragment
+import treehou.se.habit.ui.BaseFragment
+import treehou.se.habit.ui.bindings.BindingsFragment
+import treehou.se.habit.ui.colorpicker.ColorpickerActivity
+import treehou.se.habit.ui.control.*
+import treehou.se.habit.ui.control.cells.config.CellButtonConfigFragment
+import treehou.se.habit.ui.control.cells.config.CellIncDecConfigFragment
+import treehou.se.habit.ui.control.cells.config.CellSliderConfigFragment
+import treehou.se.habit.ui.control.cells.config.CellVoiceConfigFragment
+import treehou.se.habit.ui.homescreen.ControllerWidget
+import treehou.se.habit.ui.homescreen.ControllerWidgetConfigureActivity
+import treehou.se.habit.ui.homescreen.VoiceService
+import treehou.se.habit.ui.links.LinksListFragment
+import treehou.se.habit.ui.menu.NavigationDrawerFragment
+import treehou.se.habit.ui.servers.ServerMenuFragment
+import treehou.se.habit.ui.util.IconPickerActivity
+import treehou.se.habit.ui.widgets.factories.switches.RollerShutterWidgetHolder
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [(AndroidModule::class), (AppBindingModule::class)])
+interface ApplicationComponent {
+    fun inject(application: HabitApplication)
+
+    fun inject(fragment: BaseFragment)
+
+    fun inject(fragment: BaseDialogFragment)
+
+    fun inject(fragment: ControllsFragment)
+
+    fun inject(activity: ColorpickerActivity)
+
+    fun inject(activity: BaseActivity)
+
+    fun inject(activity: ControllerWidgetConfigureActivity)
+
+    fun inject(activity: EditControllerSettingsActivity)
+
+    fun inject(activity: IconPickerActivity)
+
+    fun inject(fragment: CellButtonConfigFragment)
+
+    fun inject(fragment: NavigationDrawerFragment)
+
+    fun inject(fragment: IncDecActionFragment)
+
+    fun inject(fragment: CellVoiceConfigFragment)
+
+    fun inject(fragment: SwitchActionFragment)
+
+    fun inject(fragment: BindingsFragment)
+
+    fun inject(fragment: ServerMenuFragment)
+
+    fun inject(fragment: CellIncDecConfigFragment)
+
+    fun inject(fragment: CellSliderConfigFragment)
+
+    fun inject(holder: RollerShutterWidgetHolder)
+
+    fun inject(activity: SliderActivity)
+
+    fun inject(fragment: SliderActivity.SliderFragment)
+
+    fun inject(fragment: EditControlFragment)
+
+    fun inject(fragment: CommandActionFragment)
+
+    fun inject(fragment: LinksListFragment)
+
+    fun inject(fragment: ControlFragment)
+
+    fun inject(service: VoiceActionService)
+
+    fun inject(service: VoiceService)
+
+    fun inject(service: CommandService)
+
+    fun inject(receiver: RestartBroadcastReceiver)
+
+    fun inject(provider: ControllerWidget)
+
+    fun inject(receiver: CommandReciever)
+
+    fun inject(receiver: IncDecReciever)
+}
