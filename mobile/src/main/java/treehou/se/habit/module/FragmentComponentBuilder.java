beb85eb7649297d11df18f054c66a26154b2cab9
ibaton <mattias.markehed@gmail.com>
2018-01-25 06:48:42 +0100

    Converted to kotlin

--- mobile/src/main/java/treehou/se/habit/module/FragmentComponentBuilder.java
+++
@@ -1,6 +0,0 @@
-package treehou.se.habit.module;
-
-public interface FragmentComponentBuilder<M extends ViewModule, C extends FragmentComponent> {
-    FragmentComponentBuilder<M, C> fragmentModule(M fragmentModule);
-    C build();
-}
