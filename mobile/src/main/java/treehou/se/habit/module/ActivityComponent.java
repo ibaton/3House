e009ebae5c7a0910a8a514921b411df91cf38048
ibaton <mattias.markehed@gmail.com>
2018-01-25 06:53:42 +0100

    Converted to kotlin

--- mobile/src/main/java/treehou/se/habit/module/ActivityComponent.java
+++
@@ -1,9 +0,0 @@
-package treehou.se.habit.module;
-
-
-import android.app.Activity;
-
-import dagger.MembersInjector;
-
-public interface ActivityComponent<A extends Activity> extends MembersInjector<A> {
-}
--- mobile/src/main/java/treehou/se/habit/module/FragmentComponent.java
+++
@@ -1,9 +0,0 @@
-package treehou.se.habit.module;
-
-
-import android.support.v4.app.Fragment;
-
-import dagger.MembersInjector;
-
-public interface FragmentComponent<A extends Fragment> extends MembersInjector<A> {
-}
