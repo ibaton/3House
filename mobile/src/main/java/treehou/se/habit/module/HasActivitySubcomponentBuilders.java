1f86894d76ec5dcac5119ed586dc9444ec397d77
ibaton <mattias.markehed@gmail.com>
2018-01-25 06:43:42 +0100

    Converted to kotlin

--- mobile/src/main/java/treehou/se/habit/module/HasActivitySubcomponentBuilders.java
+++
@@ -1,9 +0,0 @@
-package treehou.se.habit.module;
-
-import android.app.Activity;
-import android.support.v4.app.Fragment;
-
-public interface HasActivitySubcomponentBuilders {
-    ActivityComponentBuilder getActivityComponentBuilder(Class<? extends Activity> activityClass);
-    FragmentComponentBuilder getFragmentComponentBuilder(Class<? extends Fragment> fragmentClass);
-}
