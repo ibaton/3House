da197ce21186fc280336db0bb9b2a5a343f6d4e6
ibaton <mattias.markehed@gmail.com>
2018-03-11 13:20:15 +0100

    Fix layout problem with create servers fragment on small screens. Cleanup of create server fragment.

--- mobile/src/main/java/treehou/se/habit/ui/servers/create/CreateServerFragment.kt
+++ mobile/src/main/java/treehou/se/habit/ui/servers/create/CreateServerFragment.kt
@@ -20,14 +20,6 @@ class CreateServerFragment : Fragment() {
 
     @BindView(R.id.background) lateinit var background: View
     @BindView(R.id.create_server_options) lateinit var createServerOptions: View
-    @BindView(R.id.my_openhab_text) lateinit var myOpenhabText: View
-
-    @BindView(R.id.my_openhab_icon) lateinit var myOpenhabIcon: View
-
-    @BindView(R.id.add_my_openhab) lateinit var addMyOpenhabButton: View
-    @BindView(R.id.scan_for_servers) lateinit var scanForServersButton: View
-    @BindView(R.id.add_new_server) lateinit var newServerButton: View
-    @BindViews(R.id.add_my_openhab, R.id.scan_for_servers, R.id.add_new_server) lateinit var buttons: List<@JvmSuppressWildcards View>
 
     var introFinished = false
 
@@ -37,7 +29,7 @@ class CreateServerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_create_server, container, false)
     }
 
