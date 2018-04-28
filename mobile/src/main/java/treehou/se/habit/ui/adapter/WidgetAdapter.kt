1385f6b0fb94b82a44d25304f498e304e630cac1
ibaton <mattias.markehed@gmail.com>
2018-04-28 15:17:50 +0200

    Added slider widget

--- mobile/src/main/java/treehou/se/habit/ui/adapter/WidgetAdapter.kt
+++ mobile/src/main/java/treehou/se/habit/ui/adapter/WidgetAdapter.kt
@@ -21,6 +21,7 @@ class WidgetAdapter @Inject constructor() : RecyclerView.Adapter<WidgetAdapter.W
     @Inject lateinit var frameWidgetFactory: WidgetFrameFactory
     @Inject lateinit var textWidgetFactory: WidgetTextFactory
     @Inject lateinit var colorPickerWidgetFactory: WidgetColorpickerFactory
+    @Inject lateinit var sliderWidgetFactory: WidgetSliderFactory
 
     abstract class WidgetViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         abstract fun bind(widget: OHWidget)
@@ -59,6 +60,7 @@ class WidgetAdapter @Inject constructor() : RecyclerView.Adapter<WidgetAdapter.W
             ITEM_TYPE_FRAME -> frameWidgetFactory.createViewHolder(parent)
             ITEM_TYPE_TEXT -> textWidgetFactory.createViewHolder(parent)
             ITEM_TYPE_COLORPICKER -> colorPickerWidgetFactory.createViewHolder(parent)
+            ITEM_TYPE_SLIDER -> sliderWidgetFactory.createViewHolder(parent)
             else -> nullWidgetFactory.createViewHolder(parent)
         }
     }
@@ -77,6 +79,7 @@ class WidgetAdapter @Inject constructor() : RecyclerView.Adapter<WidgetAdapter.W
             OHWidget.WIDGET_TYPE_FRAME -> ITEM_TYPE_FRAME
             OHWidget.WIDGET_TYPE_TEXT -> ITEM_TYPE_TEXT
             OHWidget.WIDGET_TYPE_COLORPICKER -> ITEM_TYPE_COLORPICKER
+            OHWidget.WIDGET_TYPE_SLIDER -> ITEM_TYPE_SLIDER
             OHWidget.WIDGET_TYPE_SWITCH -> {
                 if (item.mapping.isEmpty()) {
                     ITEM_TYPE_SWITCH
@@ -112,5 +115,6 @@ class WidgetAdapter @Inject constructor() : RecyclerView.Adapter<WidgetAdapter.W
         val ITEM_TYPE_FRAME = 3
         val ITEM_TYPE_TEXT = 4
         val ITEM_TYPE_COLORPICKER = 5
+        val ITEM_TYPE_SLIDER = 6
     }
 }
---
+++ mobile/src/main/java/treehou/se/habit/ui/widget/WidgetSliderFactory.kt
@@ -0,0 +1,71 @@
+package treehou.se.habit.ui.widget
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+import se.treehou.ng.ohcommunicator.connector.models.OHLinkedPage
+import se.treehou.ng.ohcommunicator.connector.models.OHServer
+import se.treehou.ng.ohcommunicator.connector.models.OHWidget
+import se.treehou.ng.ohcommunicator.services.IServerHandler
+import treehou.se.habit.R
+import treehou.se.habit.ui.adapter.WidgetAdapter
+import treehou.se.habit.util.logging.Logger
+import javax.inject.Inject
+
+class WidgetSliderFactory @Inject constructor() : WidgetFactory {
+
+    @Inject lateinit var logger: Logger
+    @Inject lateinit var server: OHServer
+    @Inject lateinit var page: OHLinkedPage
+    @Inject lateinit var serverHandler: IServerHandler
+
+    override fun createViewHolder(parent: ViewGroup): WidgetAdapter.WidgetViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.widget_slider, parent, false)
+        return SliderWidgetViewHolder(view)
+    }
+
+    inner class SliderWidgetViewHolder(view: View) : WidgetBaseHolder(view, server, page) {
+
+        private lateinit var widget: OHWidget
+        val seekBarView: SeekBar = view.findViewById(R.id.dimmerSeekBar)
+
+        override fun bind(widget: OHWidget) {
+            super.bind(widget)
+            this.widget = widget
+            updateSeekBar()
+        }
+
+        private fun updateSeekBar() {
+            seekBarView.setOnSeekBarChangeListener(null)
+            try {
+                if (widget.item != null) {
+                    val progress = java.lang.Float.valueOf(widget.item.state)!!
+                    seekBarView.progress = progress.toInt()
+                }
+            } catch (e: Exception) {
+                logger.e(TAG, "Failed to update progress", e)
+                seekBarView.progress = 0
+            }
+
+            seekBarView.setOnSeekBarChangeListener(seekBarChangeListener)
+        }
+
+        private val seekBarChangeListener = object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {}
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                if (widget.item != null) {
+                    serverHandler.sendCommand(widget.item.name, seekBarView.progress.toString())
+                }
+            }
+        }
+
+    }
+
+    companion object {
+        const val TAG = "WidgetSwitchFactory"
+    }
+}
